0e5e253fb1a35b4ae9f1d9ba4adbcba07d1e5564
sherin200 <sherinsebastian21>
2022-12-03 21:12:49 +0530

    add home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeCollegeComponent } from './home-college/home-college.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCollegeComponent
   ],
   imports: [
     BrowserModule,
