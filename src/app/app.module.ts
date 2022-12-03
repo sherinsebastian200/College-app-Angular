91b03c24df5fef9ff18b91c6c0a5af8775b105bc
sherin200 <sherinsebastian21>
2022-12-03 21:34:34 +0530

    add contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeCollegeComponent } from './home-college/home-college.component';
+import { ContactCollegeComponent } from './contact-college/contact-college.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeCollegeComponent
+    HomeCollegeComponent,
+    ContactCollegeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-college/contact-college.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-college',
+  templateUrl: './contact-college.component.html',
+  styleUrls: ['./contact-college.component.css']
+})
+export class ContactCollegeComponent {
+
+}
